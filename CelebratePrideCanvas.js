d480f28a2661903cef5cf175982a4526f7084fdb
Zzbaivong <baivong@users.noreply.github.com>
2015-06-29 02:34:33 +0700

    dragdrop & up file

--- CelebratePrideCanvas.js
+++ CelebratePrideCanvas.js
@@ -1,7 +1,45 @@
+/*  @preserve
+ *  Project: Celebrate Pride Canvas
+ *  Description: Puts a rainbow filter over your image like Facebook 'Celebrate Pride'.
+ *  Author: Zzbaivong (devs.forumvi.com)
+ *  Version: 0.2
+ *  License: MIT
+ */
+
 /**
- * Celebrate Pride Canvas by Zzbaivong (devs.forumvi.com)
+ * Get image data
+ * @param {Array} files 
+ */
+function readerImage(files) {
+    if (files.length && files[0].type.indexOf('image/') === 0) {
+        var reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = function (_file) {
+            generator(_file.target.result);
+        };
+    }
+}
+
+/**
+ * Rainbow image gen
+ * @param {String} url Image url
+ */
+function generator(url) {
+    $wrap_img.addClass('generator');
+    rainbowLGBT(url, 320, function (img) {
+        $wrap_img.html('<a href="' + img + '" download="Celebratepride.png"><img src="' + img + '" alt="Celebratepride.png"></a>');
+        $download.attr('href', img);
+        $input.prop('disable', false).val('');
+        $add.slideUp('fast');
+        $complete.slideDown('fast');
+        $submit.removeAttr('style');
+    });
+}
+
+/**
+ * Puts a filter over image
  * @param {String} url      Image URL
- * @param {String} callback Export Image URL base64
+ * @param {String} callback Export to base64 image
  */
 function rainbowLGBT(url, cw, callback) {
     var img = new Image();
@@ -52,6 +90,7 @@ function rainbowLGBT(url, cw, callback) {
     };
     img.src = url;
 }
+
 var $wrap_img = $('#celebrate-img'),
     $add = $('#celebrate-add'),
     $input = $('#celebrate-input'),
@@ -60,32 +99,57 @@ var $wrap_img = $('#celebrate-img'),
     $reset = $('#celebrate-reset'),
     $download = $('#celebrate-download');
 
-$add.on('submit', function (e) {
-    e.preventDefault();
+$add.on('submit', function (event) {
+    event.preventDefault();
     $input.prop('disable', true);
     $submit.css('backgroundImage', 'url(img/load.gif)');
-    rainbowLGBT($input.val(), 320, function (img) {
-        $wrap_img.html('<a href="' + img + '" download="Celebratepride.png"><img src="' + img + '" alt="Celebratepride.png"></a>');
-        $download.attr('href', img);
-        $input.prop('disable', false).val('');
-        $add.slideUp('fast');
-        $complete.slideDown('fast');
-        $submit.removeAttr('style');
-    });
+    generator($input.val());
 });
+
 $input.on('input', function () {
     var val = $input.val();
     if ($.trim(val) === '') {
         $submit.css('backgroundImage', 'url(img/enter.png)');
-    } else if (/^https?:\/\/.+$/.test(val)) {
+    } else if (/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(val)) {
         $submit.css('backgroundImage', 'url(img/ok.png)');
     } else {
         $submit.css('backgroundImage', 'url(img/cancel.png)');
     }
 });
+
 $reset.on('click', function () {
-    $wrap_img.height(320).empty();
+    $wrap_img.height(320).html('<div><strong>Drop image</strong><br><span>(or click)</span></div>').removeClass('generator');
     $add.slideDown('fast');
     $complete.slideUp('fast');
     $submit.removeAttr('style');
+});
+
+$wrap_img.on('dragenter', function () {
+    event.preventDefault();
+    $wrap_img.addClass('dragging');
+});
+
+$wrap_img.on('dragover', function (event) {
+    $wrap_img.addClass('dragging');
+    event.preventDefault();
+});
+
+$wrap_img.on('dragleave', function (event) {
+    event.preventDefault();
+    $wrap_img.removeClass('dragging');
+});
+
+$wrap_img.on('click', function() {
+    $('#celebrate-file').click();
+});
+
+$wrap_img.on('drop dragdrop', function (event) {
+    event.preventDefault();
+    $wrap_img.removeClass('dragging');
+    readerImage(event.originalEvent.dataTransfer.files);
+});
+
+$('html').on('change', '#celebrate-file', function () {
+    readerImage(this.files);
+    $('#celebrate-file').replaceWith('<input id="celebrate-file" type="file" accept="image/*">');
 });
